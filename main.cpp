554ccfd7b9a0ea46eeaa75ab4c1c4051a7133965
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-11-10 20:42:18 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <gl/glew.h>				
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h> 
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+#pragma warning(disable: 4711 4710 4100)
