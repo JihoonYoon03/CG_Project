be5b8b8eb1496fea4a610989b71d8fc90bfd968d
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-11-22 17:09:20 +0900

    Add: Target 및 콜라이더 초안 작성

--- Targets.cpp
+++ Targets.cpp
@@ -1,1 +1,6 @@
 #include "Targets.h"
+
+TargetDefault::TargetDefault(const std::string& filename, const glm::vec3& size, const glm::vec3& defColor, const CollideMode& collider)
+	: Model(filename, size, defColor, collider) {
+
+}
